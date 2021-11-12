243f62e7a34789ca795aafbbabb383db7b4cde56
DayanandaChittoor <seenu.daya5@gmail.com>
2021-11-12 22:30:11 +0530

    Added: Stopwatch program and print elapsed time

---
+++ StopWatch.py
@@ -0,0 +1,30 @@
+'''
+@Author: Dayananda C
+@Date: 2021-10-11 
+@Time:  18:00:00
+@Last Modified by: Dayananda C
+@Title : Coupon Number
+'''
+
+import time 
+
+class stopWatch:
+    def elapsedTime() :
+        try:
+            start = int(input("To start press 1:"))
+            startTime = time.time()
+            print(startTime) 
+                
+            stop =int(input("\npress 0 to stop:"))
+            stopTime = time.time()
+            print(stopTime)
+
+            Elapsed = stopTime - startTime
+            print("\nTime Elapsed of stopwatch is :", Elapsed,'sec' ) 
+            
+        except ValueError:
+            print("You have an Exception")
+
+        
+print("Welcome to Stopwatch Program \n")
+stopWatch.elapsedTime()
